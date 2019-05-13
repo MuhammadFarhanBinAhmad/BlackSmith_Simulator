5de7bbad9a20cfcd7574f6fa5b38c051437b73cb
Farhan.A <farhan11637@gmail.com>
2019-05-13 12:49:25 +0800

    SystemProto

---
+++ BlackSmithSimulator/Assets/BlackSmithProto(1)/Scripts/Assembly/Assembly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assembly : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ore")
+        {
+            if (other.GetComponent<Ore>().current_Stage == 5)
+            {
+                other.transform.SetParent(this.gameObject.transform);
+            }
+        }
+    }
+}
