cb8d16883b96baba4680f9e294d4948d370a52ea
Farhan.A <farhan11637@gmail.com>
2019-05-16 15:20:47 +0800

    Liquid and Enchantment

---
+++ BlackSmithSimulator/Assets/Farhan's Scripts/Scripts/Potion/PotionContent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionContent : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            other.GetComponent<ThisWeaponData>().this_Enchantment_Percentage++;
+            Destroy(this.gameObject);
+            print("HIt");
+        }
+    }
+}
--- BlackSmithSimulator/Assets/Farhan's Scripts/Scripts/Weapon/ThisWeaponData.cs
+++ BlackSmithSimulator/Assets/Farhan's Scripts/Scripts/Weapon/ThisWeaponData.cs
@@ -10,6 +10,9 @@ public class ThisWeaponData : MonoBehaviour
     public int this_Weapon_Type;
     public int this_Material_Type;
     public int this_Enchantment_Type;
+    //enchantment percentage
+    public int this_Enchantment_Percentage;
+    public bool is_Enchanted;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,15 @@ public class ThisWeaponData : MonoBehaviour
         this_Weapon_Type = the_Weapon_Data.weapon_Type;
         this_Material_Type = the_Weapon_Data.weapon_Material;
         this_Enchantment_Type = the_Weapon_Data.weapon_Enchantment;
+        this_Enchantment_Percentage = 0;
+    }
+    private void FixedUpdate()
+    {
+        if (this_Enchantment_Percentage >= 100)
+        {
+            is_Enchanted = true;
+            print("True");
+        }
     }
 
 }
