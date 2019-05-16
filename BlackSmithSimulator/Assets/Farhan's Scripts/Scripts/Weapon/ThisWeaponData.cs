899ce87fcf0ac0cf6f0aefdea3815df1d7ab67b5
Farhan.A <farhan11637@gmail.com>
2019-05-16 13:50:52 +0800

    IM AM HEAVY WEAPONS GUYS
    AND THIS IS MY WEAPON

---
+++ BlackSmithSimulator/Assets/Farhan's Scripts/Scripts/Weapon/ThisWeaponData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisWeaponData : MonoBehaviour
+{
+    //attach Scriptable object
+    public WeaponData the_Weapon_Data;
+    //this object data
+    public int this_Weapon_Type;
+    public int this_Material_Type;
+    public int this_Enchantment_Type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this_Weapon_Type = the_Weapon_Data.weapon_Type;
+        this_Material_Type = the_Weapon_Data.weapon_Material;
+        this_Enchantment_Type = the_Weapon_Data.weapon_Enchantment;
+    }
+
+}
