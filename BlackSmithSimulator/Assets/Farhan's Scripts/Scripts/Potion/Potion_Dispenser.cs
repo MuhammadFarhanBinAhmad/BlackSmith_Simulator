82d19a39c57ae1df2df3a1fc24a2d838dbe2b07a
Farhan.A <farhan11637@gmail.com>
2019-05-19 15:31:48 +0800

    Potion and Effect... Done

---
+++ BlackSmithSimulator/Assets/Farhan's Scripts/Scripts/Potion/Potion_Dispenser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion_Dispenser : MonoBehaviour
+{
+    public GameObject liquid_Box_Collider;//detect collider for enchantment to go
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //input control here
+        if (Input.GetMouseButton(0))
+        {
+            GameObject liquid_Content = Instantiate(liquid_Box_Collider, transform.position, transform.rotation);//spawn enchantment potion
+        }
+    }
+}
