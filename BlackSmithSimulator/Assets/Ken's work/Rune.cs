809d3829436ef9f873616ca3971b0e5ba74411c9
songkah <songkah657@gmail.com>
2019-05-22 11:39:07 +0800

    test making

---
+++ BlackSmithSimulator/Assets/Ken's work/Rune.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+    public int Rune_types;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BlackSmithSimulator/Assets/Ken's work/runeTypes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runeTypes : MonoBehaviour
+{
+    public GameObject Rune;
+    public Transform home;
+    // Start timer when rune oj exit hitbox
+    private void OntriggerExit(Collider other)
+    {
+        StartCoroutine(Runetimer(home));
+    }
+    // delay for new rune
+    IEnumerator Runetimer (Transform home)
+    {
+        yield return new WaitForSeconds(3f);
+        Instantiate(Rune);
+        print("New rune");
+    }
+}
