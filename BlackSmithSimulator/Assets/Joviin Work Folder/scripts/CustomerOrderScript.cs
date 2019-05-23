741dfbaa412b4089b713d81c23fe7a95ae7ac25d
Blackbirdexe <cjoviin@yahoo.com>
2019-05-23 20:56:17 +0800

    Customer

--- BlackSmithSimulator/Assets/Joviin Work Folder/scripts/CustomerOrderScript.cs
+++ BlackSmithSimulator/Assets/Joviin Work Folder/scripts/CustomerOrderScript.cs
@@ -1,36 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomerOrderScript : MonoBehaviour
 {
     //To be attached to AI spawner(above the counter)
 
-    public GameObject[] weaponBroken;
-    public int[] numberToGive;
+    //public GameObject[] weaponBroken;
+    //public int[] numberToGive;
+    public Text requestedMaterialText;
+    public Text requestedWeaponText;
+    public Text requestedEnchantmentText;
+
+    int requestedMaterial;
+    int requestedWeapon;
+    int requestedEnchantment;
 
 
     private void Start()
     {
-        GiveWeapon(numberToGive[0]);
+        //GiveWeapon(numberToGive[0]);
+        CreateNewRequest();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //If (other.getComponent<Weapon>() != null)
+        if (other.GetComponent<ThisWeaponData>() != null)
+        {
+            RecieveWeapon(other.gameObject);
+        }
     }
 
     void GiveWeapon(int numberToSpawn)
     {
         for (int i = 0; i < numberToSpawn; i++)
         {
-            Instantiate(weaponBroken[0], this.transform.position, this.transform.rotation);
+            //Instantiate(weaponBroken[0], this.transform.position, this.transform.rotation);
         }
  
     }
 
-    void RecieveWeapon()
+    void RecieveWeapon(GameObject weaponCollected)
     {
+        ThisWeaponData weaponCollectedTemp = weaponCollected.GetComponent<ThisWeaponData>();
+        if (weaponCollectedTemp.this_Material_Type == requestedMaterial &&
+            weaponCollectedTemp.this_Weapon_Type == requestedWeapon && 
+            weaponCollectedTemp.this_Enchantment_Type == requestedEnchantment)
+        {
+            //Correct case
+            print("Weapon Ok! Moving on to next request");
+            CreateNewRequest();
+        }
+        else
+        {
+            //Wrong case
+        }
 
+    }
+
+    void CreateNewRequest()
+    {
+        requestedMaterial = Random.Range(1, 5);
+        requestedWeapon = Random.Range(1, 5);
+        requestedEnchantment = Random.Range(1, 7);
+
+        requestedMaterialText.text = requestedMaterial.ToString();
+        requestedWeaponText.text = requestedWeapon.ToString();
+        requestedEnchantmentText.text = requestedEnchantment.ToString();
     }
 }
