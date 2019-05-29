97cc41630a62e97d5b3630c78350cf7df925722d
Farhan.A <farhan11637@gmail.com>
2019-05-29 23:47:46 +0800

    scene Transition

---
+++ BlackSmithSimulator/Assets/CatBugStuff/Farhan's Scripts/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string scene;
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
