4f07d8909f5bb627f2d7e608f9f5bbdf4d0ee696
Farhan.A <farhan11637@gmail.com>
2019-11-08 22:48:01 +0800

    THE DOOR

---
+++ BlackSmithSimulator/Assets/Joviin Work Folder/scripts/EndDayCloseDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK.Controllables;
+using UnityEngine.UI;
+
+public class EndDayCloseDoor : MonoBehaviour
+{
+    public VRTK_BaseControllable controllable;
+    public Text displayText;
+    public string outputOnMax = "Maximum Reached";
+    public string outputOnMin = "Minimum Reached";
+    Scene current_Scene;
+
+    private void Start()
+    {
+        current_Scene = SceneManager.GetActiveScene();
+    }
+
+    protected virtual void OnEnable()
+    {
+        controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+        controllable.ValueChanged += ValueChanged;
+        controllable.MaxLimitReached += MaxLimitReached;
+        controllable.MinLimitReached += MinLimitReached;
+    }
+
+    protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+    {
+
+    }
+
+    protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+    {
+    }
+    protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+    {
+        if (current_Scene.name == "Pause_Main_Menu")
+        {
+            SceneManager.LoadScene("Game_Level");
+        }
+    }
+}
---
+++ BlackSmithSimulator/Assets/Joviin Work Folder/scripts/StartDayOpenDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK.Controllables;
+using UnityEngine.UI;
+
+public class StartDayOpenDoor : MonoBehaviour
+{
+    public VRTK_BaseControllable controllable;
+    public Text displayText;
+    public string outputOnMax = "Maximum Reached";
+    public string outputOnMin = "Minimum Reached";
+    Scene current_Scene;
+
+    private void Start()
+    {
+        current_Scene = SceneManager.GetActiveScene();
+    }
+
+    protected virtual void OnEnable()
+    {
+        controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+        controllable.ValueChanged += ValueChanged;
+        controllable.MaxLimitReached += MaxLimitReached;
+        controllable.MinLimitReached += MinLimitReached;
+    }
+
+    protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+    {
+
+    }
+
+    protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+    {
+ 
+        if (current_Scene.name == "EndOfDay")
+        {
+            SceneManager.LoadScene("Pause_Main_Menu");
+        }
+
+    }
+    protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+    {
+        if (current_Scene.name == "Pause_Main_Menu")
+        {
+            SceneManager.LoadScene("Game_Level");
+        }
+    }
+}
+
