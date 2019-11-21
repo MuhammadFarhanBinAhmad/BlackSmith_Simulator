537006f591dfe1442fa37b066d72d4d8af2e93cc
Farhan.A <farhan11637@gmail.com>
2019-11-21 16:48:13 +0800

    VFX

--- BlackSmithSimulator/Assets/StoreAssets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
+++ BlackSmithSimulator/Assets/StoreAssets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class SpawnEffect : MonoBehaviour {
 
     public float spawnEffectTime = 2;
-    public float pause = 1;
     public AnimationCurve fadeIn;
 
-    ParticleSystem ps;
+    //ParticleSystem ps;
     float timer = 0;
     Renderer _renderer;
 
@@ -18,28 +17,16 @@ public class SpawnEffect : MonoBehaviour {
     {
         shaderProperty = Shader.PropertyToID("_cutoff");
         _renderer = GetComponent<Renderer>();
-        ps = GetComponentInChildren <ParticleSystem>();
-
-        var main = ps.main;
-        main.duration = spawnEffectTime;
-
-        ps.Play();
 
     }
 	
 	void Update ()
     {
-        if (timer < spawnEffectTime + pause)
+        if (timer <= spawnEffectTime)
         {
             timer += Time.deltaTime;
-        }
-        else
-        {
-            ps.Play();
-            timer = 0;
-        }
 
-
+        }
         _renderer.material.SetFloat(shaderProperty, fadeIn.Evaluate( Mathf.InverseLerp(0, spawnEffectTime, timer)));
         
     }
