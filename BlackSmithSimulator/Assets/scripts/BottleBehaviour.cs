df0a2c8adc46f061080e60f26684a6477fd5ea4e
Joviin Charles <cjoviin@yahoo.com>
2019-11-28 17:44:38 +0800

    201911281744

---
+++ BlackSmithSimulator/Assets/scripts/BottleBehaviour.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK.Controllables;
+
+public class BottleBehaviour : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    bool isEnabled;
+    public GameObject dropEffect;
+
+    public GameobjectType objectType;
+    public InteractableUIEffect UIEffect;
+
+
+    public enum GameobjectType
+    {
+        Prop,
+        InteractableUI
+    }
+
+    public enum InteractableUIEffect
+    {
+        NoEffect,
+        Teleport,
+        MainMenu,
+        QuitGame,
+    }
+
+    private void Start()
+    {
+        if(this.gameObject.activeInHierarchy == true)
+        {
+            isEnabled = true;
+        }
+        else
+        {
+            isEnabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+        isEnabled = true;
+    }
+
+    private void OnDisable()
+    {
+        isEnabled = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            print("Relative velocity is "+collision.relativeVelocity.magnitude);
+        }
+
+        if (Mathf.Round(collision.relativeVelocity.magnitude) >= 5  && collision.gameObject.layer == 9)
+        {
+            OnDrop();
+        }
+    }
+
+    void OnDrop()
+    {
+        switch (objectType)
+        {
+            case GameobjectType.Prop:
+                print("Velocity is " + Mathf.Round(rigidBody.velocity.magnitude * rigidBody.velocity.magnitude));
+                Instantiate(dropEffect, this.transform.position, Quaternion.Euler(0, 0, 0));
+                Destroy(this.gameObject);
+                break;
+            case GameobjectType.InteractableUI:
+                print("Velocity is " + Mathf.Round(rigidBody.velocity.magnitude * rigidBody.velocity.magnitude));
+                InteractableEffect();
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+    void InteractableEffect()
+    {
+        switch (UIEffect)
+        {
+            case InteractableUIEffect.NoEffect:
+                break;
+            case InteractableUIEffect.Teleport:
+                Instantiate(dropEffect, this.transform.position, Quaternion.Euler(0, 0, 0));
+                break;
+            case InteractableUIEffect.MainMenu:
+                Instantiate(dropEffect, this.transform.position, Quaternion.Euler(0, 0, 0));
+                SceneManager.LoadScene("Pause_Main_Menu");
+                break;
+            case InteractableUIEffect.QuitGame:
+                Instantiate(dropEffect, this.transform.position, Quaternion.Euler(0, 0, 0));
+                Application.Quit();
+                break;
+        }
+    }
+}
