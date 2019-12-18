807b3d448b09c99f4a371d56778ac246c6e7b42c
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2019-12-18 22:45:43 +0800

    Change colour when touched

---
+++ BlackSmithSimulator/Assets/Scripts/ParticleEffect & others/ColourChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name =="Hand")
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(.75f, .75f, .75f, 1);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Hand")
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
