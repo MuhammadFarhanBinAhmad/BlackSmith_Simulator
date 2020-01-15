5af9cfcc2aef0156a3610429c3c62c347868419b
Blackbirdexe <49791840+Blackbirdexe@users.noreply.github.com>
2020-01-16 02:30:10 +0800

    tutorial adjustments

---
+++ BlackSmithSimulator/Assets/Scripts/Tutorial/TutorialListner.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialListner : MonoBehaviour
+{
+    public List<AudioClip> tVoicelines = new List<AudioClip>();
+    public void FlipToNextPage()
+    {
+
+    }
+
+    public void OpenRuneSquare()
+    {
+
+    }
+
+    public void CreateBaseRune1()
+    {
+
+    }
+    
+    public void PickUpBaseRune1()
+    {
+
+    }
+
+    public void PlaceBaseRune1OnRuneCombiner()
+    {
+
+    }
+
+    public void PickUpBaseRune2()
+    {
+
+    }
+
+    public void PlaceBaseRune2OnRuneCombiner()
+    {
+
+    }
+
+    public void PlaceRune1InFanvil()
+    {
+
+    }
+
+    public void PickUpRune2()
+    {
+
+    }
+
+    public void PlaceRune2InFanvil()
+    {
+
+    }
+
+    public void PlaceOreInFanvil()
+    {
+
+    }
+
+    public void UseFanvil()
+    {
+
+    }
+
+    public void WeaponOnTable()
+    {
+
+    }
+
+    public void PickUpRune3()
+    {
+
+    }
+    public void PlaceRune3OnWeapon()
+    {
+
+    }
+
+    public void RingBell()
+    {
+
+    }
+
+    public void RingBell2()
+    {
+
+    }
+}
