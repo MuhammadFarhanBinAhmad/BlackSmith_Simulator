d2e9c7a6c837d81e69c582091686720ba505ec96
Blackbirdexe <49791840+Blackbirdexe@users.noreply.github.com>
2020-02-05 16:42:16 +0800

    abang

---
+++ BlackSmithSimulator/Assets/Scripts/testscene/subtitletest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class subtitletest : MonoBehaviour
+{
+    public Text textBox;
+    public TextMeshProUGUI tmProGUI;
+    
+
+    private void Start()
+    {
+        InvokeRepeating("PrintText", 1, 2);
+    }
+
+    void PrintText()
+    {
+        //textBox.text = textBox.text + ("this is the first line \n this is the second line \n");
+        tmProGUI.text = tmProGUI.text + ("this is the first line \n this is the second line \n");
+
+        if (tmProGUI.text.Length > 1)
+        {
+
+        }
+    }
+}
